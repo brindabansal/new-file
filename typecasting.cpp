fa8e2d6ad2f2344561ddf9322455cf917fff3aa3
Brinda bansal <bansalshikha487@gmail.com>
2023-07-25 12:13:53 +0530

    Add files via upload

---
+++ typecasting.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{     //learning typecasting
+    int a ='a';
+    cout<<a<<endl;
+    //it gave you value 97,this a is tycasted that is changed from char to int
+
+    char x=98;
+    cout<<x<<endl;
+    //here you saw that we found ki kiski value 98 hai..so it gave b as output
+}
---
+++ variable type.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a= 4;
+    cout << "value of a is"<<a<<endl;
+
+    float b=1.23;
+    cout<<"value of b is"<<b<<endl;
+
+    bool v= false;
+    cout<<v<<endl;
+    
+
+    char char_is='b';
+    //here remember that you cannot only one letter work inside single inverted commas
+    cout<<"character is"<<endl;
+
+    float size= sizeof(a);
+    cout<<"size of a is:"<< size<<endl;
+    return 0;
+
+
+
+}
